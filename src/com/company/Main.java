1f75a3c2c4356859c7124070af885b4d6f7edebb
al5091 <angelovinolim@gmail.com>
2018-10-05 11:20:08 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Jimmy Hopkins", 2.67);
+        Student student2 = new Student("Harry Potter", 4.59);
+        Student student3 = new Student("Lindsay Weir", 4.96);
+        Student student4 = new Student("Jimmy Brooks", 3.51);
+        Student student5 = new Student("Claire Standish", 4.02);
+
+
+    }
+}
---
+++ src/com/company/Student.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by al5091 on 10/5/18.
+ */
+public class Student {
+
+    public double weightedAverage;
+    public Student(String name, double GPA) {
+
+    }
+
+}
